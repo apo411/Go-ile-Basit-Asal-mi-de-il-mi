b92db6102dc6119234e01e57953bfc30316031d0
apoo <72878063+apo411@users.noreply.github.com>
2022-07-02 14:19:19 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 
+package loops
+
+import "fmt"
+
+
+
+func proje() {
+	sayi := 0
+	fmt.Println("Bir sayı giriniz.")
+	fmt.Scanln(&sayi)
+
+	asalMi := true
+
+	for i := 2; i < sayi; i++ {
+		if sayi%i == 0 {
+			asalMi = false
+		}
+
+	}
+	if asalMi == true {
+		fmt.Println("Asaldır")
+
+	} else {
+		fmt.Println("Asal Değildir")
+	}
+}
